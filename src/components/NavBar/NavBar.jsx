2fb8bc2fc1e84cb45fcf19da03e55bde31a89af5
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-09 15:58:11 +0430

    update the path navBar menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,33 +5,51 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import "./NavBar.css"
+import "./NavBar.css";
 import { Margin } from "@mui/icons-material";
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="proBackground">
       <Container>
-        <Navbar.Brand href="#" className="kar">Kar+</Navbar.Brand>
+        <Navbar.Brand href="#" className="kar">
+          Kar+
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0 nav-container"
             style={{ maxHeight: "100px" }}
             navbarScroll>
-            <Link to="/findTalent" style={{textDecoration:"none",margin:"0 1px",}}>Find Talent</Link>
-            <Link to="/findWork" style={{textDecoration:"none",margin:"0 10px",}}>Find Work</Link>
-            <Link to="/companies" style={{textDecoration:"none",margin:"0 10px",}}>Companies</Link>
-            <Link to="/aboutUs" style={{textDecoration:"none",margin:"0 10px",}}>About us</Link>
+            <Link
+              to="/find-talent"
+              style={{ textDecoration: "none", margin: "0 1px" }}>
+              Find Talent
+            </Link>
+            <Link
+              to="/find-work"
+              style={{ textDecoration: "none", margin: "0 10px" }}>
+              Find Work
+            </Link>
+            <Link
+              to="/companies"
+              style={{ textDecoration: "none", margin: "0 10px" }}>
+              Companies
+            </Link>
+            <Link
+              to="/about-us"
+              style={{ textDecoration: "none", margin: "0 10px" }}>
+              About us
+            </Link>
           </Nav>
           <Form className="d-flex">
-            <Link to="signIn">
-            <Button variant="outline-success" className="mx-4">
-              Sign in
-            </Button>
+            <Link to="signin">
+              <Button variant="outline-success" className="mx-4">
+                Sign in
+              </Button>
             </Link>
-            <Link to="signUp">
-            <Button variant="outline-success">Sign up</Button>
+            <Link to="signup">
+              <Button variant="outline-success">Sign up</Button>
             </Link>
           </Form>
         </Navbar.Collapse>
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
 import LandingPage from "./pages/LandingPage/LandingPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import AboutUs from "./allMenus/aboutUs/aboutUs";
-import Companies from "./allMenus/companies/companies";
-import FindTalent from "./allMenus/findTalent/findTakent";
-import SignIn from "./allMenus/signIn/signIn";
-import SignUp from "./allMenus/signUp/signUp";
-import FindWork from "./allMenus/findWork/findWork";
+import AboutUs from "./pages/aboutUs/aboutUs";
+import Companies from "./pages/companies/companies";
+import FindTalent from "./pages/findTalent/findTakent";
+import SignIn from "./pages/signIn/signIn";
+import SignUp from "./pages/signUp/signUp";
+import FindWork from "./pages/findWork/findWork";
 
 let routes = [
   { path: "/", element: <LandingPage /> },
-  { path: "/login", element: <LoginPage /> },
   { path: "/about-us", element: <AboutUs /> },
   { path: "/find-work", element: <FindWork /> },
   { path: "/find-talent", element: <FindTalent /> },
