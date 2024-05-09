261c2649af968380561de8942539003d8bc12cb7
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-09 16:19:06 +0430

    wrap the work carts

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,36 +1,38 @@
-import React, { useState } from 'react'
-import './searchBar.css'
-import jobData from '../cards/jobCards/jobList'
-import JobCard from '../cards/jobCards/jobCard'
+import React, { useState } from "react";
+import "./searchBar.css";
+import jobData from "../cards/jobCards/jobList";
+import JobCard from "../cards/jobCards/jobCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function SearchBar() {
   return (
     <div>
-      <p className='first-content'>Find your dream job, Build your 
-           network or Hire the best talents!</p>
-           <div className='box-container'>
-           <div className='searchBox'>
-            <input type="text" placeholder='search...' />
-            <span>|
+      <p className="first-content">
+        Find your dream job, Build your network or Hire the best talents!
+      </p>
+      <div className="box-container">
+        <div className="searchBox">
+          <input type="text" placeholder="search..." />
+          <span>
+            |
             <select name="select">
-                <option value="talent">talent</option>
-                <option value="job">job</option>
+              <option value="talent">talent</option>
+              <option value="job">job</option>
             </select>
-            </span>
-            
-           </div>
-           
-              <button>Search</button>
-                </div>
-                <div className='card-container'>
-                  {jobData.map(data=>(
-                     <JobCard {...data} key={data.id}/>
-                  ))}
-                
-                </div>
+          </span>
+        </div>
 
-                <div className="card-container"></div>
-
+        <button>Search</button>
+      </div>
+      <Container>
+        <Row>
+          {jobData.map((data) => (
+            <Col className="col-4">
+              <JobCard {...data} key={data.id} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
