5349af91d63abfea79e70fe268996c807b090eb3
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-11 19:43:44 +0430

    update all change

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./LandingPage.css";
 import Header from "../../components/Header/Header";
 import LastJobCarts from "../../components/LastJobCarts/LastJobCarts";
+import AvailableJob from "../../components/available-job/availableJob";
 
 export default function LandingPage() {
   return (
     <>
       <Header />
-      <LastJobCarts />
+      <AvailableJob />
     </>
   );
 }
