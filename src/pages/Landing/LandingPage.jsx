0fa9403f6bbfea867ef103e12019821b5adf6d19
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-11 19:47:34 +0430

    update landing page

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./LandingPage.css";
 import Header from "../../components/Header/Header";
-
 import AvailableJob from "../../components/available-job/availableJob";
 
 export default function LandingPage() {
