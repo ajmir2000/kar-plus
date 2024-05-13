9e7f1a871d6099a1f1074fd799abe9dfad3ce407
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-13 22:37:01 +0430

    convert css classes of search bar to bootstrap classes

--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -3,6 +3,7 @@ import "./searchBar.css";
 import { Container, Col, Row, Button } from "react-bootstrap";
 import jobData from "../cards/jobCards/jobList";
 import JobCard from "../cards/jobCards/jobCard";
+import { FiChevronDown } from "react-icons/fi";
 
 export default function SearchBar() {
   const [selectState, setSelectState] = useState("talebt");
@@ -14,70 +15,52 @@ export default function SearchBar() {
   };
 
   return (
-    // <div>
-    //   <div className="box-container">
-    //     <div className="searchBox">
-    //       <input type="text" placeholder="search..." />
-    //       <span>
-    //         |
-    //         <select
-    //           name="select"
-    //           onChange={(event) => handleSelectChange(event)}>
-    //           <option value="talent">talent</option>
-    //           <option value="job">job</option>
-    //         </select>
-    //       </span>
-    //     </div>
-
-    //     <button>Search</button>
-    //   </div>
-    //   {selectState === "job" ? (
-    //     <Container>
-    //       <Row>
-    //         {jobData
-    //           .filter((data) => data.id <= 3)
-    //           .map((data) => (
-    //             <Col className="col-3 my-5 ">
-    //               <JobCard {...data} key={data.id} />
-    //             </Col>
-    //           ))}
-    //       </Row>
-    //     </Container>
-    //   ) : null}
-    // </div>
     <div>
-      <div class="d-flex justify-content-between align-items-center">
-        <div class="d-flex searchBox">
-          <input type="text" class="form-control" placeholder="search..." />
-          <span class="mx-2">
-            |
+      <div className="d-flex align-items-center ms-5">
+        <div className="d-flex ms-5 p-3  w-50  border-1 rounded-5 custom-background-color ">
+          <input
+            type="text"
+            className="form-control bg-transparent border-0 text-light w-75 mx-3 custom-focus-outline custom-placeholder-color"
+            placeholder="search..."
+          />
+          <span className=" d-flex ">
+            <span className="d-flex align-items-center  text-light ">|</span>
             <select
-              class="form-control"
+              className="form-control d-flex align-items-center  text-light  bg-transparent border-0  custom-focus-outline"
               name="select"
               onChange={(event) => handleSelectChange(event)}>
-              <option value="talent">talent</option>
-              <option value="job">job</option>
+              <option value="talent" className="text-dark bg-transparent ">
+                talent
+              </option>
+              <option value="job" className="text-dark bg-transparent">
+                job
+              </option>
             </select>
+          </span>
+          <span className="text-light d-flex  align-items-center justify-content-center custom-arrow-down">
+            <FiChevronDown />
           </span>
         </div>
 
-        <Button className="rounded-5 bg-white text-dark border-0">
+        <Button className="rounded-5 bg-white text-dark border-0 ms-4 p-3 ">
           Search
         </Button>
       </div>
-      {selectState === "job" ? (
-        <div class="container">
-          <div class="row">
-            {jobData
-              .filter((data) => data.id <= 3)
-              .map((data) => (
-                <div class="col-3 my-5 ">
-                  <JobCard {...data} key={data.id} />
-                </div>
-              ))}
+      <div>
+        {selectState === "job" ? (
+          <div class="container">
+            <div class="row">
+              {jobData
+                .filter((data) => data.id <= 3)
+                .map((data) => (
+                  <div class="col-3 my-5 ">
+                    <JobCard {...data} key={data.id} />
+                  </div>
+                ))}
+            </div>
           </div>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
     </div>
   );
 }
