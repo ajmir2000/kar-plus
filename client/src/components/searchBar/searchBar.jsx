18b3a84af4bb2099e49c72cd6d9a64c4942ba7ce
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-14 16:06:03 +0430

    fix css problem of search bar

--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -1,32 +1,30 @@
 import React, { useState } from "react";
 import "./searchBar.css";
-import { Container, Col, Row, Button } from "react-bootstrap";
+import { Container, Button, FormSelect } from "react-bootstrap";
 import jobData from "../cards/jobCards/jobList";
 import JobCard from "../cards/jobCards/jobCard";
-import { FiChevronDown } from "react-icons/fi";
 
 export default function SearchBar() {
   const [selectState, setSelectState] = useState("talebt");
 
   const handleSelectChange = (event) => {
     setSelectState(event.target.value);
-
-    console.log(selectState);
   };
 
   return (
-    <div>
-      <div className="d-flex align-items-center ms-5">
-        <div className="d-flex ms-5 p-3  w-50  border-1 rounded-5 custom-background-color ">
+    <Container className="px-5">
+      <div className="d-flex align-items-center ms-5 row">
+        <div className="d-flex ms-5 p-2  w-75  border-1 rounded-5  custom-background-color  col-11 ">
           <input
             type="text"
             className="form-control bg-transparent border-0 text-light w-75 mx-3 custom-focus-outline custom-placeholder-color"
             placeholder="search..."
           />
-          <span className=" d-flex ">
+          <span className=" d-flex ps-4 ">
             <span className="d-flex align-items-center  text-light ">|</span>
+
             <select
-              className="form-control d-flex align-items-center  text-light  bg-transparent border-0  custom-focus-outline"
+              className="form-select-lg d-flex align-items-center  text-light  bg-transparent border-0  custom-focus-outline"
               name="select"
               onChange={(event) => handleSelectChange(event)}>
               <option value="talent" className="text-dark bg-transparent ">
@@ -37,12 +35,9 @@ export default function SearchBar() {
               </option>
             </select>
           </span>
-          <span className="text-light d-flex  align-items-center justify-content-center custom-arrow-down">
-            <FiChevronDown />
-          </span>
         </div>
 
-        <Button className="rounded-5 bg-white text-dark border-0 ms-4 p-3 ">
+        <Button className="rounded-5 bg-white text-dark border-0 ms-4 py-3 col-1">
           Search
         </Button>
       </div>
@@ -61,6 +56,6 @@ export default function SearchBar() {
           </div>
         ) : null}
       </div>
-    </div>
+    </Container>
   );
 }
