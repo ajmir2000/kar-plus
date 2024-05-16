ad57a66ae0e2695855ef3af833a5814543df0884
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-16 20:01:05 +0430

    complete UI of pagination Part

--- client/src/components/PaginationPart/PaginationPart.jsx
+++ client/src/components/PaginationPart/PaginationPart.jsx
@@ -4,18 +4,16 @@ import "./PaginationPart.css";
 
 export default function PaginationPart() {
   return (
-    <Pagination className="d-flex justify-content-center mt-3 ">
-      <Pagination.First />
-      <Pagination.Prev />
-      <Pagination.Item active>{1}</Pagination.Item>
-      <Pagination.Item>{2}</Pagination.Item>
-      <Pagination.Item>{3}</Pagination.Item>
-      <Pagination.Item>{4}</Pagination.Item>
-      <Pagination.Item>{5}</Pagination.Item>
-      <Pagination.Ellipsis />
-      <Pagination.Item>{20}</Pagination.Item>
-      <Pagination.Next />
-      <Pagination.Last />
+    <Pagination className="d-flex justify-content-center mt-3">
+      <Pagination.Prev   className="mx-1 custom-pagination-left-arrows"/>
+      <Pagination.Item   className="mx-1 custom-pagination-numbers active ">{1}</Pagination.Item>
+      <Pagination.Item  className="mx-1 custom-pagination-numbers">{2}</Pagination.Item>
+      <Pagination.Item  className="mx-1 custom-pagination-numbers">{3}</Pagination.Item>
+      <Pagination.Item  className="mx-1 custom-pagination-numbers">{4}</Pagination.Item>
+      <Pagination.Item  className="mx-1 custom-pagination-numbers">{5}</Pagination.Item>
+      <Pagination.Ellipsis  className="mx-1 custom-pagination-numbers" />
+      <Pagination.Item  className="mx-1 custom-pagination-numbers">{20}</Pagination.Item>
+      <Pagination.Next   className="mx-1 custom-pagination-right-arrows"/>
     </Pagination>
   );
 }
