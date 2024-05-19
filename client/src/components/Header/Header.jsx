296ffe713ddcd1462af7052d8dfae229011c5319
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-05-19 15:44:20 +0430

    Header make resposive for mobile, tablet and computer

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,36 +8,42 @@ export default function Header() {
   return (
     <>
       <Container>
-        <span className="row">
-          <p className="col-1"></p>
-          <h3 className=" d-flex justify-content-center col-6 ms-5 text-white mb-4 ">
-            Find your dream job, Build your network or Hire the best talents!
-          </h3>
-        </span>
+        <div class="row align-items-center">
+          <div class="col-1 d-none d-md-block"></div>
+          <div class="col-12 col-md-6 ms-md-5 text-center text-md-start mb-4 mb-md-0">
+            <h3 class="text-white">
+              Find your dream job, Build your network or Hire the best talents!
+            </h3>
+          </div>
+        </div>
       </Container>
 
       <SearchBar />
 
       {/* this is popular  component and it has custom-padding class these styles are on Header.css flie */}
-      <Container>
-        <div className="row ">
-          <div className="col-1"></div>
-          <div className="col-10 d-flex m-auto ps-4 my-2">
-            <Nav className="me-auto my-2 my-lg-0 nav-container" navbarScroll>
-              <Link to="#" className="nav-link text-light custom-padding">
+
+      <Container className="d-felx justify-content-center ">
+        <div className="row d-felx justify-content-center">
+          <div className="col-12 col-md-1"></div>
+          <div className="col-12 col-md-10 d-flex justify-content-center my-2">
+            <Nav
+              className="me-auto my-2 my-lg-0 nav-container d-flex flex-wrap justify-content-center"
+              navbarScroll>
+              <Link
+                to="#"
+                className="nav-link text-light custom-padding d-none d-md-inline">
                 Popular :
               </Link>
-              <Link to="#" className="nav-link   text-light custom-padding">
+              <Link to="#" className="nav-link text-light custom-padding">
                 Graphic Designer
               </Link>
-
-              <Link to="#" className="nav-link  text-light custom-padding">
+              <Link to="#" className="nav-link text-light custom-padding">
                 Web Developer
               </Link>
-              <Link to="#" className="nav-link  text-light custom-padding">
+              <Link to="#" className="nav-link text-light custom-padding">
                 IT Manager
               </Link>
-              <Link to="#" className="nav-link  text-light custom-padding">
+              <Link to="#" className="nav-link text-light custom-padding">
                 Digital Markter
               </Link>
             </Nav>
--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -12,53 +12,58 @@ export default function SearchBar() {
   };
 
   return (
-    <Container className="px-5">
-      <div className="d-flex align-items-center mx-5 row">
-        <div className="d-flex ms-5 p-2  w-75  border-1 rounded-5  custom-background-color  col-11 ">
-          <input
-            type="text"
-            className="form-control bg-transparent border-0 text-light w-75 mx-3 custom-focus-outline custom-placeholder-color"
-            placeholder="search..."
-          />
-          <span className=" d-flex ps-4 ">
-            <span className="d-flex align-items-center  text-light ">|</span>
-
-            <select
-              className="form-select-lg d-flex align-items-center  text-light  bg-transparent border-0  custom-focus-outline"
-              name="select"
-              onChange={(event) => handleSelectChange(event)}>
-              <option value="talent" className="text-dark bg-transparent ">
-                talent
-              </option>
-              <option value="job" className="text-dark bg-transparent">
-                job
-              </option>
-            </select>
-          </span>
-        </div>
-
-        <Button className="rounded-5 bg-white text-dark border-0 ms-4 py-3  col-2">
-          <h5 className="d-flex align-items-center  justify-content-center  m-auto ">
-            {" "}
-            Search
-          </h5>
-        </Button>
-      </div>
-      <div>
-        {selectState === "job" ? (
-          <div class="container">
-            <div class="row">
-              {jobData
-                .filter((data) => data.id <= 3)
-                .map((data) => (
-                  <div class="col-3 my-5 mx-4 ">
-                    <JobCard {...data} key={data.id} />
-                  </div>
-                ))}
+    <>
+      <Container className="px-5">
+        <div className="row mx-5 align-items-center">
+          <div className="col-12 col-md-9">
+            <div className="d-flex flex-md-row align-items-center ms-md-5 p-2 border-1 rounded-5 custom-background-color">
+              <input
+                type="text"
+                className="form-control bg-transparent border-0 text-light w-100 w-md-75 mx-3 custom-focus-outline custom-placeholder-color"
+                placeholder="search..."
+              />
+              <span className="d-flex ps-4">
+                <span className="d-flex align-items-center text-light">|</span>
+                <select
+                  className="form-select-lg d-flex align-items-center text-light bg-transparent border-0 custom-focus-outline"
+                  name="select"
+                  onChange={(event) => handleSelectChange(event)}>
+                  <option value="talent" className="text-dark bg-transparent">
+                    talent
+                  </option>
+                  <option value="job" className="text-dark bg-transparent">
+                    job
+                  </option>
+                </select>
+              </span>
             </div>
           </div>
-        ) : null}
-      </div>
-    </Container>
+          <div className="col-12 col-md-3  mt-3 mt-md-0 d-flex justify-content-center">
+            <Button className="rounded-5 bg-white text-dark border-0 py-3 w-100">
+              <h5 className="d-flex align-items-center justify-content-center m-auto">
+                Search
+              </h5>
+            </Button>
+          </div>
+        </div>
+        <div>
+          {selectState === "job" ? (
+            <div className="container">
+              <div className="row">
+                {jobData
+                  .filter((data) => data.id <= 3)
+                  .map((data) => (
+                    <div
+                      className="col-12 col-sm-6 col-md-4 col-lg-3 my-5 mx-4"
+                      key={data.id}>
+                      <JobCard {...data} />
+                    </div>
+                  ))}
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </Container>
+    </>
   );
 }
