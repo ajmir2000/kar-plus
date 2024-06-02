0a7b47e25c1a4f88c080f39aeaddfea24d89e2a4
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-06-02 19:44:18 +0430

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,39 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
   res.json({ message: "api route is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+  try {
+    // if user want change the password , password must hashed
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        // set is method check if the data ( uername , password, email, photo) being is changed to change otherwise going to ignore that data, like if user change just photo  database update  the photo and  ( uername , password, email) are not changed, in simple user can update any data  want.
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      //save and update new user information if new: fasle it retrun old data of user.
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) return next(errorHandler(401, "Unauthorized"));
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Forbidden"));
+    // user parameter is just the ID of user becuse we select the id of user when we make the cookie
+    req.user = user;
+    next();
+  });
+};
