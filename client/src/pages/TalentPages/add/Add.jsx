f358543cd9863c479002a7715b5f7fb2fe9bf6de
ajmir <ajmirfaqiri2000.af@gmail.com>
2024-07-28 00:03:53 +0430

    add responsive functionality on add gig page

--- client/src/pages/TalentPages/add/Add.jsx
+++ client/src/pages/TalentPages/add/Add.jsx
@@ -204,63 +204,61 @@ const Add = () => {
   };
 
   return (
-    <div className="add bg-white">
-      <div className="container">
-        <form onSubmit={handleSubmit(onSubmit)} className="container">
+    <div class="add bg-white">
+      <div class="container">
+        <form onSubmit={handleSubmit(onSubmit)} class="container">
           <h1>Add New Gig</h1>
-          <div className="row">
-            <div className="col-md-6">
-              <label className="form-label mb-2">Title</label>
+          <div class="row">
+            <div class="col-12 col-md-6 ">
+              <label class="form-label mb-2">Title</label>
               <input
                 placeholder="Logo Design"
                 {...register("title", { required: "Gig Title is required" })}
-                className={`form-control ${errors.title ? "is-invalid" : ""}`}
+                class={`form-control ${errors.title ? "is-invalid" : ""}`}
               />
               {errors.title && (
-                <div className="invalid-feedback">{errors.title.message}</div>
+                <div class="invalid-feedback">{errors.title.message}</div>
               )}
             </div>
-            <div className="col-md-6">
-              <label className="form-label mb-2">Category</label>
+            <div class="col-12 col-md-6">
+              <label class="form-label mb-2">Category</label>
               <select
                 {...register("cat", {
                   required: "Category is required",
                 })}
-                className="form-control">
+                class="form-control">
                 <option value="design">Design</option>
                 <option value="web">Web Development</option>
                 <option value="animation">Animation</option>
                 <option value="graphicDesign">Graphic Design</option>
               </select>
               {errors.category && (
-                <div className="invalid-feedback">
-                  {errors.category.message}
-                </div>
+                <div class="invalid-feedback">{errors.category.message}</div>
               )}
             </div>
-            <div className="col-md-6 mt-3">
-              <label htmlFor="companyLogo" className="form-label">
+            <div class="col-12 col-md-6 mt-3">
+              <label for="companyLogo" class="form-label">
                 Cover
               </label>
               <input
                 type="file"
-                className="form-control"
+                class="form-control"
                 onChange={handleFileChange}
                 accept=".png,.jpg,.jpeg"
               />
               {filePerc > 0 && `Upload ${filePerc}%`}
               {fileUploadError && (
-                <div className="text-danger">
+                <div class="text-danger">
                   Please upload a valid image file (PNG or JPEG).
                 </div>
               )}
             </div>
 
             {/*start Portfolio Images  */}
-            <div className="d-flex gap-4">
+            <div class="col-12 d-flex gap-4 mt-3">
               <input
                 onChange={(e) => setFiles(e.target.files)}
-                className="p-3 border border-secondary rounded w-100"
+                class="p-3 border border-secondary rounded w-100"
                 type="file"
                 id="images"
                 accept="image/*"
@@ -270,39 +268,39 @@ const Add = () => {
                 type="button"
                 disabled={uploading}
                 onClick={handleImageSubmit}
-                className={`p-3 text-success border border-success rounded text-uppercase ${
+                class={`p-3 text-success border border-success rounded text-uppercase ${
                   uploading ? "opacity-80" : "hover-shadow-lg"
                 }`}>
                 {uploading ? "Uploading..." : "Upload"}
               </button>
             </div>
-            <p className="text-danger small">
+            <p class="col-12 text-danger small">
               {imageUploadError && imageUploadError}
             </p>
             {formData.images?.length > 0 &&
               formData.images.map((url, index) => (
                 <div
                   key={url}
-                  className="d-flex justify-content-between  border align-items-center">
+                  class="col-12 d-flex justify-content-between  border align-items-center mt-3">
                   <img
                     src={url}
                     alt="listing image"
-                    className="w-25  h-25 object-contain rounded-lg"
+                    class="w-25  h-25 object-contain rounded-lg"
                   />
                   <button
                     type="button"
                     onClick={() => handleRemoveImage(index)}
-                    className="p-3 text-danger rounded text-uppercase hover-opacity-75">
+                    class="p-3 text-danger rounded text-uppercase hover-opacity-75">
                     Delete
                   </button>
                 </div>
               ))}
 
             {/*end Portfolio Images  */}
-            <div className="col-12 mt-3">
-              <label className="form-label mb-2">Description</label>
+            <div class="col-12 mt-3">
+              <label class="form-label mb-2">Description</label>
               <textarea
-                className={`form-control p-3 ${
+                class={`form-control p-3 ${
                   errors.description ? "is-invalid" : ""
                 }`}
                 rows={4}
@@ -312,33 +310,27 @@ const Add = () => {
                 placeholder="Brief descriptions to introduce your service to customers"
               />
               {errors.description && (
-                <div className="invalid-feedback">
-                  {errors.description.message}
-                </div>
+                <div class="invalid-feedback">{errors.description.message}</div>
               )}
             </div>
-            <div className="col-12 mt-3">
-              <label className="form-label mb-2">Service Title</label>
+            <div class="col-12 mt-3">
+              <label class="form-label mb-2">Service Title</label>
               <input
                 placeholder="e.g. One-page web design"
                 name="shortTitle"
                 {...register("shortTitle", {
                   required: "shortTitle is required",
                 })}
-                className={`form-control ${
-                  errors.shortTitle ? "is-invalid" : ""
-                }`}
+                class={`form-control ${errors.shortTitle ? "is-invalid" : ""}`}
               />
               {errors.shortTitle && (
-                <div className="invalid-feedback">
-                  {errors.shortTitle.message}
-                </div>
+                <div class="invalid-feedback">{errors.shortTitle.message}</div>
               )}
             </div>
-            <div className="col-12 mt-3">
-              <label className="form-label mb-2">Short Description</label>
+            <div class="col-12 mt-3">
+              <label class="form-label mb-2">Short Description</label>
               <textarea
-                className={`form-control p-3 ${
+                class={`form-control p-3 ${
                   errors.description ? "is-invalid" : ""
                 }`}
                 rows={2}
@@ -348,53 +340,49 @@ const Add = () => {
                 placeholder="Short description of your service"
               />
               {errors.shortDesc && (
-                <div className="invalid-feedback">
-                  {errors.shortDesc.message}
-                </div>
+                <div class="invalid-feedback">{errors.shortDesc.message}</div>
               )}
             </div>
-            <div className="col-md-6 mt-3">
-              <label className="form-label mb-2">
-                Delivery Time (e.g. 3 days)
-              </label>
+            <div class="col-12 col-md-6 mt-3">
+              <label class="form-label mb-2">Delivery Time (e.g. 3 days)</label>
               <input
                 type="number"
                 {...register("deliveryTime", {
                   required: "Delivery Time are required",
                 })}
-                className={`form-control ${
+                class={`form-control ${
                   errors.deliveryTime ? "is-invalid" : ""
                 }`}
               />
               {errors.deliveryTime && (
-                <div className="invalid-feedback">
+                <div class="invalid-feedback">
                   {errors.deliveryTime.message}
                 </div>
               )}
             </div>
-            <div className="col-md-6 mt-3">
-              <label className="form-label mb-2">Revision Number</label>
+            <div class="col-12 col-md-6 mt-3">
+              <label class="form-label mb-2">Revision Number</label>
               <input
                 type="number"
                 {...register("revisionNumber", {
                   required: "Revision Number are required",
                 })}
-                className={`form-control ${
+                class={`form-control ${
                   errors.revisionNumber ? "is-invalid" : ""
                 }`}
               />
               {errors.revisionNumber && (
-                <div className="invalid-feedback">
+                <div class="invalid-feedback">
                   {errors.revisionNumber.message}
                 </div>
               )}
             </div>
 
-            <div className="col-12 mt-3">
-              <label htmlFor="features">Add Features</label>
-              <label className="form-label mb-2">Features:</label>
+            <div class="col-12 mt-3">
+              <label for="features">Add Features</label>
+              <label class="form-label mb-2">Features:</label>
               <CreatableSelect
-                className="form-control py-4"
+                class="form-control py-4"
                 defaultValue={selectedOption}
                 onChange={setSelectedOption}
                 options={options}
@@ -402,21 +390,21 @@ const Add = () => {
               />
             </div>
 
-            <div className="col-md-6 mt-3">
-              <label className="form-label mb-2">Price</label>
+            <div class="col-12 col-md-6 mt-3">
+              <label class="form-label mb-2">Price</label>
               <input
                 type="number"
                 {...register("price", {
                   required: "Price are required",
                 })}
-                className={`form-control ${errors.price ? "is-invalid" : ""}`}
+                class={`form-control ${errors.price ? "is-invalid" : ""}`}
               />
               {errors.price && (
-                <div className="invalid-feedback">{errors.price.message}</div>
+                <div class="invalid-feedback">{errors.price.message}</div>
               )}
             </div>
-            <div className="col-12 mt-4">
-              <button type="submit" className="btn btn-primary">
+            <div class="col-12 mt-4">
+              <button type="submit" class="btn btn-primary">
                 Create
               </button>
             </div>
